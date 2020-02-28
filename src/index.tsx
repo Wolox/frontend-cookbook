59b222e78c138a2e6f7ce5188aaaf6003f271c78
David Rojas <david_rojas82142@elpoli.edu.co>
2020-02-28 16:18:08 -0500

    bootstrap react proyect

---
+++ src/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
+import App from './app';
+import './config/i18n';
+import './scss/application.scss';
+import { register } from './serviceWorker';
+
+const render = () => {
+  ReactDOM.render(
+    <AppContainer>
+      <App />
+    </AppContainer>,
+    document.getElementById('root')
+  );
+};
+
+// Render once
+render();
+
+register();
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    render();
+  });
+}
