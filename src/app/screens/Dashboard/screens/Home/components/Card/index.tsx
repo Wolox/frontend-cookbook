001e5d2ce2e2021ffa30d22e73da36c39c14c6c5
Dorian Alvarado <dorianalvarado@MacBook-Pro-de-wolox.local>
2020-04-26 15:26:43 -0300

    Built a useShadow hook to render components from github and implemented it

--- src/app/screens/Dashboard/screens/Home/components/Card/index.tsx
+++ src/app/screens/Dashboard/screens/Home/components/Card/index.tsx
@@ -1,35 +1,38 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+
+import Routes from '~constants/routes';
+import useShadow from '~hooks/useShadow';
 
 import { COLORS } from './constants';
 import styles from './styles.module.scss';
 
 interface Props {
   component: {
-    title: string,
-    html: string,
-    css: string
-  },
-  number: number,
-  category: string
+    title: string;
+    html: string;
+    css: string;
+  };
+  number: number;
+  category: string;
 }
 
 function Card({ component, number }: Props) {
-  useEffect(() => {
-    const elem = document.querySelector(`#${component.title}`);
-    const shadowRoot = elem?.attachShadow({ mode: 'open' });
-    if (shadowRoot) {
-      shadowRoot.innerHTML = `${component.html}<style>${component.css}</style>`;
-    };
-  }, [component.css, component.html, component.title]);
+  const { html, css } = component;
+  const { category } = useParams();
+  const shadowElem = useShadow<HTMLDivElement>({ html, css });
+  const linkRoute = Routes.DETAIL.replace(':category', category as string).replace(
+    ':component',
+    component.title
+  );
 
   const cardColor = { '--card-color': COLORS[Math.floor(number % COLORS.length)] } as React.CSSProperties;
   return (
     <div className={`column middle center full-width ${styles.card}`} style={cardColor}>
-      <div className={`full-width row middle center ${styles.cardContent}`} id={component.title} />
+      <div ref={shadowElem} className={`full-width row middle center ${styles.cardContent}`} />
       <div className={`full-width column ${styles.cardInfo}`}>
         <h4 className={`m-bottom-2 ${styles.cardTitle}`}>{component.title}</h4>
-        <Link to='/'>Watch more...</Link>
+        <Link to={linkRoute}>Watch more...</Link>
       </div>
     </div>
   );
