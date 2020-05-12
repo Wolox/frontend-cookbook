fbd6f1cf842588ec5e7568c2ed459b04676754b7
dvoraSSantacruz <saritachavita@hotmail.com>
2020-05-11 23:55:51 -0500

    change setBaseUrl to a new api instance

--- src/services/AuthServices.js
+++ src/services/AuthServices.js
@@ -1,6 +1,8 @@
-import api from '../config/api';
+import { create } from 'apisauce';
 
 import * as LocalStorageService from './LocalStorageService';
+
+const api = create({ baseURL: process.env.REACT_APP_AUTH_BASE_URL });
 
 export const setCurrentUser = currentUser => {
   api.setHeader('Authorization', currentUser.sessionToken);
@@ -18,10 +20,8 @@ export const getCurrentUser = () => {
 
 export const removeCurrentUser = () => LocalStorageService.removeSessionToken();
 
-export const loginToGithub = code => {
-  const authApi = api.setBaseURL(`${process.env.REACT_APP_AUTH_BASE_URL}?code=${code}`);
-  return authApi.post().then(response => {
+export const loginToGithub = code =>
+  api.post(`?code=${code}`).then(response => {
     setCurrentUser({ sessionToken: response });
     window.history.replaceState({}, document.title, '/');
   });
-};
