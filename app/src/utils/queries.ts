cd701626d902b80cffe41d1deb9f1ebb1561471b
Pablo Ferro <pablo.pyp@gmail.com>
2020-05-18 10:31:28 -0300

    Add web tech

--- app/src/utils/queries.ts
+++ app/src/utils/queries.ts
@@ -2,10 +2,12 @@ import { gql } from 'apollo-boost';
 
 const RECIPES_BRANCH = process.env.REACT_APP_RECIPES_BRANCH || 'components';
 
+const tech = 'react';
+
 const queryBuilder = (name: string, target = '', query: string) => gql`
   query ${name} {
     repository(owner: "wolox", name: "frontend-cookbook") {
-      object(expression: "${RECIPES_BRANCH}:components${target}") {
+      object(expression: "${RECIPES_BRANCH}:recipes-${tech}/components${target}") {
         ${query}
       }
     }
