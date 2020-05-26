fd4e548892a94919daa5f00441b4e41fff07bf60
Oli <olivia.fernandez@wolox.com.ar>
2020-05-26 12:42:13 -0300

    Add disabled styles.

--- cookbook-react/recipes/inputs/input-3/index.tsx
+++ cookbook-react/recipes/inputs/input-3/index.tsx
@@ -5,15 +5,16 @@ import './styles.scss';
 
 interface Props {
   disabled?: boolean;
-  error?: boolean;
+  error?: string;
 }
 
 function Input({ disabled, error }: Props) {
   return (
     <div id="input-3" className="input-container">
       <div className="input-wrapper">
-        <input className={cn("input-base", {"input-error": true})} placeholder=" " disabled={disabled} />
-        <span className="input-label">Label</span>
+        <input className={cn("input-base", { "input-error": !!error, "input-disabled": disabled })} placeholder=" " disabled={disabled} />
+        <span className={cn("input-label", { "label-disabled": disabled })}>Label</span>
+        {!!error && <span className="error-message">{error}</span>}
       </div>
     </div>
   );
