755c1f0ede535ea083474574d7c78d405774e6ff
Oli <olivia.fernandez@wolox.com.ar>
2020-05-26 11:50:32 -0300

    Add error styles.

--- cookbook-react/recipes/inputs/input-3/index.tsx
+++ cookbook-react/recipes/inputs/input-3/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import cn from 'classnames';
 
 import './styles.scss';
 
 interface Props {
-  disable?: boolean;
+  disabled?: boolean;
   error?: boolean;
 }
 
-function Input({ disable, error }: Props) {
+function Input({ disabled, error }: Props) {
   return (
     <div id="input-3" className="input-container">
       <div className="input-wrapper">
-        <input className="input-base" placeholder=" "/>
+        <input className={cn("input-base", {"input-error": true})} placeholder=" " disabled={disabled} />
         <span className="input-label">Label</span>
       </div>
     </div>
