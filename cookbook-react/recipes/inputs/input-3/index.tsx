9d250b4bf70c89450fa56355e4af2a1a5bfe2ead
Oli <olivia.fernandez@wolox.com.ar>
2020-05-26 09:59:29 -0300

    Fixes on input-3 styles.

--- cookbook-react/recipes/inputs/input-3/index.tsx
+++ cookbook-react/recipes/inputs/input-3/index.tsx
@@ -11,9 +11,8 @@ function Input({ disable, error }: Props) {
   return (
     <div id="input-3" className="input-container">
       <div className="input-wrapper">
-
-        <input className="input-base" placeholder="Label" />
-        <label className="input-label">Label</label>
+        <input className="input-base" placeholder=" "/>
+        <span className="input-label">Label</span>
       </div>
     </div>
   );
