94a180ba8cde9363b96405a1c556331c46773871
Oli <olivia.fernandez@wolox.com.ar>
2020-05-28 12:10:28 -0300

    Styling tab bar.

--- cookbook-react/recipes/tab-bars/tab-bar-1/index.tsx
+++ cookbook-react/recipes/tab-bars/tab-bar-1/index.tsx
@@ -4,14 +4,15 @@ import cn from 'classnames';
 import './styles.scss';
 
 function TabBar() {
-  const [selection, setSelection] = useState(null);
+  const [selection, setSelection] = useState(0);
 
   return (
     <div id="tab-bar-1" className="tab-bar-container">
       <div className="tab-bar-wrapper">
-          <button className={cn("option", {"selected": selection == 0})} onClick={() => setSelection(0)}>Sección 1</button>
-          <button className={cn("option", {"selected": selection == 1})} onClick={() => setSelection(1)}>Sección 2</button>
-          <button className={cn("option", {"selected": selection == 2})} onClick={() => setSelection(2)}>Sección 3</button>
+        <span className={`selector selected-${selection}`} />
+        <button className={cn("option", { "selected": selection == 0 })} onClick={() => setSelection(0)}>Sección 1</button>
+        <button className={cn("option", { "selected": selection == 1 })} onClick={() => setSelection(1)}>Sección 2</button>
+        <button className={cn("option", { "selected": selection == 2 })} onClick={() => setSelection(2)}>Sección 3</button>
       </div>
     </div>
   );
