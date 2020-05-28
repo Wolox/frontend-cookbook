378bce0931e5bcb9be14eed7801037b978566f81
Oli <olivia.fernandez@wolox.com.ar>
2020-05-28 12:50:25 -0300

    Orginize code.

--- cookbook-react/recipes/tab-bars/tab-bar-1/index.tsx
+++ cookbook-react/recipes/tab-bars/tab-bar-1/index.tsx
@@ -10,9 +10,24 @@ function TabBar() {
     <div id="tab-bar-1" className="tab-bar-container">
       <div className="tab-bar-wrapper">
         <span className={`selector selected-${selection}`} />
-        <button className={cn("option", { "selected": selection == 0 })} onClick={() => setSelection(0)}>Sección 1</button>
-        <button className={cn("option", { "selected": selection == 1 })} onClick={() => setSelection(1)}>Sección 2</button>
-        <button className={cn("option", { "selected": selection == 2 })} onClick={() => setSelection(2)}>Sección 3</button>
+        <button
+          className={cn("option", { "selected": selection == 0 })}
+          onClick={() => setSelection(0)}
+        >
+          Sección 1
+        </button>
+        <button
+          className={cn("option", { "selected": selection == 1 })}
+          onClick={() => setSelection(1)}
+        >
+          Sección 2
+        </button>
+        <button
+          className={cn("option", { "selected": selection == 2 })}
+          onClick={() => setSelection(2)}
+        >
+          Sección 3
+        </button>
       </div>
     </div>
   );
