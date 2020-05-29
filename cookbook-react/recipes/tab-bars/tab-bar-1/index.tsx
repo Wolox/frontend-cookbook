0a862d8a2be35b9cd22dfe99d70d5757f7a099aa
Oli <olivia.fernandez@wolox.com.ar>
2020-05-29 14:52:52 -0300

    Support adding tabs.

--- cookbook-react/recipes/tab-bars/tab-bar-1/index.tsx
+++ cookbook-react/recipes/tab-bars/tab-bar-1/index.tsx
@@ -4,30 +4,32 @@ import cn from 'classnames';
 import './styles.scss';
 
 function TabBar() {
-  const [selection, setSelection] = useState(0);
+  const [selection, setSelection] = useState(null);
 
   return (
     <div id="tab-bar-1" className="tab-bar-container">
       <div className="tab-bar-wrapper">
         <span className={`selector selected-${selection}`} />
-        <button
-          className={cn("option", { "selected": selection == 0 })}
-          onClick={() => setSelection(0)}
-        >
-          Sección 1
+        <div className="tabs">
+          <button
+            className={cn("option", { "selected": selection == 0 })}
+            onClick={() => setSelection(0)}
+          >
+            Sección 1
         </button>
-        <button
-          className={cn("option", { "selected": selection == 1 })}
-          onClick={() => setSelection(1)}
-        >
-          Sección 2
+          <button
+            className={cn("option", { "selected": selection == 1 })}
+            onClick={() => setSelection(1)}
+          >
+            Sección 2
         </button>
-        <button
-          className={cn("option", { "selected": selection == 2 })}
-          onClick={() => setSelection(2)}
-        >
-          Sección 3
+          <button
+            className={cn("option", { "selected": selection == 2 })}
+            onClick={() => setSelection(2)}
+          >
+            Sección 3
         </button>
+        </div>
       </div>
     </div>
   );
