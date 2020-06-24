f6646a297ecd2177af735be79a69f831a0427869
Oli <olivia.fernandez@wolox.com.ar>
2020-06-24 09:50:11 -0300

    Format code.

--- app/src/services/FirebaseService.js
+++ app/src/services/FirebaseService.js
@@ -3,7 +3,7 @@ import firebase from 'firebase/app'
 import app from 'config/firebase';
 
 export const updateDownloadsCounter = (tech, component) => {
-    const db = app.firestore();
-    var componentDownloads = db.collection('downloads').doc('techs').collection(tech).doc(component || 'no-name');
-    componentDownloads.set({ count: firebase.firestore.FieldValue.increment(1) },{merge: true});
+  const db = app.firestore();
+  var componentDownloads = db.collection('downloads').doc('techs').collection(tech).doc(component || 'no-name');
+  componentDownloads.set({ count: firebase.firestore.FieldValue.increment(1) }, { merge: true });
 }
