bb3c3c8ba218bcb65072902a1fbb1dc375505c0a
Martín Callegari <martin.callegari@wolox.com.ar>
2020-06-26 17:59:34 -0300

    Update app/src/services/FirebaseService.js

--- app/src/services/FirebaseService.js
+++ app/src/services/FirebaseService.js
@@ -4,6 +4,6 @@ import app from 'config/firebase';
 
 export const updateDownloadsCounter = (tech, component) => {
   const db = app.firestore();
-  var componentDownloads = db.collection('downloads').doc('techs').collection(tech).doc(component || 'no-name');
+  const componentDownloads = db.collection('downloads').doc('techs').collection(tech).doc(component || 'no-name');
   componentDownloads.set({ count: firebase.firestore.FieldValue.increment(1) }, { merge: true });
 }
