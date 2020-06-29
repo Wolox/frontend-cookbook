a23a1da06148f85ae8135339db92dc3ae77563d4
Oli <olivia.fernandez@wolox.com.ar>
2020-06-29 10:29:50 -0300

    remove unnecessary line.

--- app/src/services/FirebaseService.js
+++ app/src/services/FirebaseService.js
@@ -3,7 +3,6 @@ import firebase from 'firebase/app'
 import app from 'config/firebase';
 
 export const updateDownloadsCounter = (tech, component) => {
-  const db = app.firestore();
-  const componentDownloads = db.collection('downloads').doc('techs').collection(tech).doc(component || 'no-name');
+  const componentDownloads = app.firestore().collection('downloads').doc('techs').collection(tech).doc(component || 'no-name');
   componentDownloads.set({ count: firebase.firestore.FieldValue.increment(1) }, { merge: true });
 }
