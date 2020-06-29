ae41c0a698c2ffae5ab11a5f24485a7a27e0efc1
Oli <olivia.fernandez@wolox.com.ar>
2020-06-29 16:45:08 -0300

    copy components from cookbook-react branch.

---
+++ cookbook-react/recipes/collapses/collapse-1/index.tsx
@@ -0,0 +1,33 @@
+ 
+import React, { useState, useCallback } from 'react';
+import cn from 'classnames';
+
+import './styles.scss';
+
+interface Props {
+  title?: string;
+  text?: string;
+}
+
+function Collapse({ title, text }: Props) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div id="collapse-1" className="collapse-container">
+      <div className={cn("collapse-wrapper", { "collapse-open": open })}>
+        <div className="title-container">
+          <h2 className="title">{title}</h2>
+          <button type="button" className={cn("icon", { "icon-clicked": open })} onClick={() => setOpen(!open)}></button>
+        </div>
+        <p className="text">{text}</p>
+      </div>
+    </div>
+  );
+}
+
+Collapse.defaultProps = {
+  title: "Título / Pregunta / Ítem",
+  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+}
+
+export default Collapse;
