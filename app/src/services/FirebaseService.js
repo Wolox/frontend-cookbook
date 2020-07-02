f75838c3030d807e5b2e43e230ab229267f9d1d8
Pablo Ferro <pablo.pyp@gmail.com>
2020-07-02 12:05:39 -0300

    Fix firebase imports, avoid counting downloads without config

--- app/src/services/FirebaseService.js
+++ app/src/services/FirebaseService.js
@@ -1,13 +1,16 @@
-import firebase from 'firebase/app';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
 
 import app from 'config/firebase';
 
 export const updateDownloadsCounter = (tech, component) => {
-  const componentDownloads = app
-    .firestore()
-    .collection('downloads')
-    .doc('techs')
-    .collection(tech)
-    .doc(component || 'no-name');
-  componentDownloads.set({ count: firebase.firestore.FieldValue.increment(1) }, { merge: true });
+  if (app) {
+    const componentDownloads = app
+      .firestore()
+      .collection('downloads')
+      .doc('techs')
+      .collection(tech)
+      .doc(component || 'no-name');
+    componentDownloads.set({ count: firebase.firestore.FieldValue.increment(1) }, { merge: true });
+  }
 };
