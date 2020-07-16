4b7c655ac6775ebfe612950f833fd8f350bd4968
Pablo Ferro <pablo.pyp@gmail.com>
2020-07-16 11:35:08 -0300

    Update collapse-1 style

--- cookbook-react/recipes/collapses/collapse-1/index.tsx
+++ cookbook-react/recipes/collapses/collapse-1/index.tsx
@@ -1,4 +1,4 @@
- import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import cn from 'classnames';
 
 import './styles.scss';
@@ -13,10 +13,14 @@ function Collapse({ title, text }: Props) {
 
   return (
     <div id="collapse-1" className="collapse-container">
-      <div className={cn("collapse-wrapper", { "collapse-open": open })}>
+      <div className={cn('collapse-wrapper', { 'collapse-open': open })}>
         <div className="title-container">
           <h2 className="title">{title}</h2>
-          <button type="button" className={cn("icon", { "icon-clicked": open })} onClick={() => setOpen(!open)}></button>
+          <button
+            type="button"
+            className={cn('icon', { 'icon-clicked': open })}
+            onClick={() => setOpen(!open)}
+          />
         </div>
         <p className="text">{text}</p>
       </div>
@@ -25,8 +29,9 @@ function Collapse({ title, text }: Props) {
 }
 
 Collapse.defaultProps = {
-  title: "Título / Pregunta / Ítem",
-  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-}
+  text:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  title: 'Título / Pregunta / Ítem'
+};
 
 export default Collapse;
