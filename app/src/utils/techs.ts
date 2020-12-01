4b63450557a5eca350bcc8287e0dcd76757f03d6
Pablo Ferro <pablo.pyp@gmail.com>
2020-12-01 17:07:05 -0300

    Handle missing recipe config

--- app/src/utils/techs.ts
+++ app/src/utils/techs.ts
@@ -1,6 +1,4 @@
-import { uniq, flatten, entries } from 'lodash';
-
-import { Categories } from '~components/Sidebar/interface';
+import { uniq, flatten } from 'lodash';
 
 import { COOKBOOK_PREFIX, RECIPES_DIRECTORY, SRC_DIRECTORY } from './queries';
 
@@ -9,7 +7,7 @@ interface Directory {
   name: string;
   object?: {
     entries: Directory[];
-  }
+  };
 }
 
 export interface TechsResult {
@@ -17,7 +15,7 @@ export interface TechsResult {
     object: {
       entries: Directory[];
     };
-  }
+  };
 }
 
 export const parseTechs = (data: TechsResult) =>
@@ -33,23 +31,21 @@ export const parseTechs = (data: TechsResult) =>
 const getSubdir = (dir: Directory, subDir: string) => {
   const recipesDir = dir.object?.entries.find(entry => entry.name === subDir);
   return recipesDir?.object?.entries.map(entry => entry.name);
-}
+};
 
 export const parseCategories = (data: TechsResult) =>
   uniq(
     flatten(
       data.repository.object.entries
         .filter(entry => entry.name.startsWith(COOKBOOK_PREFIX))
-        .map(cookbook =>
-          {
-            const recipesDir = getSubdir(cookbook, RECIPES_DIRECTORY);
-            if (recipesDir) {
-              return recipesDir;
-            }
-
-            const srcDir = cookbook.object?.entries.find(entry => entry.name === SRC_DIRECTORY);
-            return srcDir && getSubdir(srcDir, RECIPES_DIRECTORY);
+        .map(cookbook => {
+          const recipesDir = getSubdir(cookbook, RECIPES_DIRECTORY);
+          if (recipesDir) {
+            return recipesDir;
           }
-        )
+
+          const srcDir = cookbook.object?.entries.find(entry => entry.name === SRC_DIRECTORY);
+          return srcDir && getSubdir(srcDir, RECIPES_DIRECTORY);
+        })
     ).filter(category => !!category) as string[]
   );
