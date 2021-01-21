ddbac509b9263c22e5985ae0cf5d1a1b231a7fbf
Pablo Ferro <pablo.pyp@gmail.com>
2021-01-21 15:29:49 -0300

    cookbook install fetches the index.tsx file

--- cli/src/commands/install.ts
+++ cli/src/commands/install.ts
@@ -1,4 +1,5 @@
 import {Command, flags} from '@oclif/command'
+import fetch from 'node-fetch'
 
 export default class Install extends Command {
   static description = 'install a recipe from the cookbook'
@@ -21,5 +22,9 @@ export default class Install extends Command {
     const {args} = this.parse(Install)
 
     this.log(`Installing ${args.tech} ${args.category}/${args.recipe}...`)
+    this.log()
+    this.log('-index.tsx-\n')
+    const file = await fetch(`https://raw.githubusercontent.com/Wolox/frontend-cookbook/master/cookbook-${args.tech}/${args.tech === 'react' ? 'src/' : ''}recipes/${args.category}/${args.recipe}/index.tsx`)
+    this.log(await file.text())
   }
 }
