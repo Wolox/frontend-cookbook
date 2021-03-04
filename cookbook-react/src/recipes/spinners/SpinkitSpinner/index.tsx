a19d40d72f1a584d36463a4fb3f0ddb70889c41c
Damián Finkelstein <dfinkelstein@wolox.com.ar>
2021-03-04 17:35:22 -0300

    added comment for spinkit docs and fixed an import

--- cookbook-react/src/recipes/spinners/SpinkitSpinner/index.tsx
+++ cookbook-react/src/recipes/spinners/SpinkitSpinner/index.tsx
@@ -3,7 +3,7 @@ import SpinkitSpinner, { SpinnerProps } from 'react-spinkit';
 
 import { COLOR_SPINNER, SPINNER_DEFAULT } from './constants';
 
-// Check http://kyleamathews.github.io/react-spinkit/ for all possible spinner styles 
+// Check http://kyleamathews.github.io/react-spinkit/ for all possible spinner styles
 function Loading({ className = '', name = SPINNER_DEFAULT, color = COLOR_SPINNER, ...rest }: SpinnerProps) {
   return <SpinkitSpinner className={className} name={name} color={color} fadeIn="half" {...rest} />;
 }
