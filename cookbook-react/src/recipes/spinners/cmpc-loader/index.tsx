6439c701a531c7d814c1ae90dd084c2cfccd545c
Emanuel Correa <emanuel.correa@wolox.com.ar>
2021-03-23 14:55:18 -0300

    react-cmpc-loader

---
+++ cookbook-react/src/recipes/spinners/cmpc-loader/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import clsx from 'clsx';
+
+import styles from './styles.module.scss';
+
+interface LoadingProps {
+  simple: boolean;
+}
+
+export default function Loader({ simple = false }: LoadingProps) {
+  return (
+    <div className={clsx(styles.loaderContainer, { [styles.simple]: simple })}>
+      <div className={styles.ldsRing}>
+        <div className={`${styles.section} ${styles.section1}`} />
+        <div className={`${styles.section} ${styles.section2}`} />
+        <div className={`${styles.section} ${styles.section3}`} />
+      </div>
+    </div>
+  );
+}
