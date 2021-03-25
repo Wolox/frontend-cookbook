8d0f26262922bb586ed483175fa496dad11e3a07
Emanuel Correa <emanuel.correa@wolox.com.ar>
2021-03-25 11:56:28 -0300

    react-table-cmpc

---
+++ cookbook-react/src/recipes/tables/cmpc-table/constants.ts
@@ -0,0 +1,18 @@
+export const data = [
+  {
+    id: 1,
+    data: 'Sample1'
+  },
+  {
+    id: 2,
+    data: 'Sample2'
+  },
+  {
+    id: 3,
+    data: 'Sample3'
+  },
+  {
+    id: 4,
+    data: 'Sample4'
+  }
+];
