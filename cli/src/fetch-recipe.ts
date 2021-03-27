db1fb117b134a4d2dac081743a9f4cff72d795f5
Pablo Ferro <pablo.pyp@gmail.com>
2021-03-27 12:36:10 -0300

    Recursive fetchRecipe

--- cli/src/fetch-recipe.ts
+++ cli/src/fetch-recipe.ts
@@ -2,41 +2,62 @@ import {Recipe} from './recipe'
 import fetch from 'node-fetch'
 const fs = require('fs').promises
 
-type RecipeFile = {
+type RecipeFile ={
+  type: 'file';
   name: string;
-  type: 'file' | 'dir';
   download_url: string;
 }
 
-const importFile = async (file: RecipeFile) => {
-  // TODO: What happens when the file isn't a text :thinking-face:
+type RecipeDir = {
+  type: 'dir';
+  name: string;
+  path: string;
+}
+
+type RecipeNode = RecipeFile | RecipeDir
+
+const COOKBOOK_BASE_URL = 'https://api.github.com/repos/Wolox/frontend-cookbook/contents'
+
+const getCookbookUrl = (tech: string) => ({
+  react: 'cookbook-react/src/recipes',
+}[tech] || `cookbook-${tech}/recipes`)
+
+const importFile = async (file: RecipeFile, currentDir = '') => {
+  // TODO: What happens when the file isn't a text 🤔
   const response = await fetch(file.download_url, {
     headers: {'Content-Type': 'application/text'},
   })
-  return fs.writeFile(`./${file.name}`, await response.text())
-}
 
-const importDirectory = async (file: RecipeFile) => {
-  const filePath = `./${file.name}`
-  await fs.mkdir(filePath, {recursive: true})
-  return null
-  // return getDir(`${BASE_URL}/${recipeData.path}`, filePath)
-}
-
-export async function fetchRecipe(recipe: Recipe) {
-  const res = await fetch(`https://api.github.com/repos/Wolox/frontend-cookbook/contents/cookbook-react/src/recipes/${recipe.category}/${recipe.name}`)
-
-  if (res.status === 404) {
-    throw new Error(`A recipe with name '${recipe.name}' and category '${recipe.category}' could not be found in cookbook-${recipe.tech}`)
-  } else if (res.status < 200 || res.status > 299) {
-    throw new Error(`The following error has occured requesting ${recipe.name}: \n ${res.json()}`)
+  if (response.status === 404) {
+    throw new Error(`The file with name '${file.name}' could not be found`)
+  } else if (response.status < 200 || response.status > 299) {
+    throw new Error(`The following error has occured requesting the file ${file.name}: \n ${response.json()}`)
   }
-  const recipeResponse = await (res.json() as Promise<RecipeFile[]>)
 
-  return Promise.all(recipeResponse.map(file => {
-    if (file.type === 'file') {
-      return importFile(file)
+  await fs.writeFile(`./${currentDir}/${file.name}`, await response.text())
+}
+
+export async function fetchRecipe(recipe: Recipe, currentDir?: string): Promise<void> {
+  if (currentDir) {
+    await fs.mkdir('.' + currentDir, {recursive: true})
+  }
+
+  const response = await fetch(`${COOKBOOK_BASE_URL}/${getCookbookUrl(recipe.tech)}/${recipe.category}/${recipe.name}${currentDir || ''}`)
+
+  if (response.status === 404) {
+    throw new Error(`A recipe with name '${recipe.name}' and category '${recipe.category}' could not be found in cookbook-${recipe.tech}`)
+  } else if (response.status < 200 || response.status > 299) {
+    throw new Error(`The following error has occured requesting ${recipe.name}: \n ${response.json()}`)
+  }
+  const recipeResponse = await (response.json() as Promise<RecipeNode[]>)
+
+  await Promise.all(recipeResponse.map(file => {
+    switch (file.type) {
+    case 'file':
+      return importFile(file, currentDir)
+    case 'dir':
+      return fetchRecipe(recipe, `${currentDir || ''}/${file.name}`)
     }
-    return importDirectory(file)
+    throw new Error(`Unknow file type ${(file as any).type}`)
   }))
 }
