71c6d477cb43f83c33ad2948a4d86164ec8e50c7
Pablo Ferro <pablo.pyp@gmail.com>
2021-03-27 14:05:26 -0300

    Update test

--- cli/test/commands/install.test.ts
+++ cli/test/commands/install.test.ts
@@ -3,10 +3,11 @@ import {expect, test} from '@oclif/test'
 describe('install', () => {
   test
   .stdout()
+  .stderr()
   .command(['install', 'react', 'buttons', 'button-1'])
   .it('runs install and shows the tech, category and recipe', ctx => {
-    expect(ctx.stdout).to.contain('react')
-    expect(ctx.stdout).to.contain('buttons')
-    expect(ctx.stdout).to.contain('button-1')
+    expect(ctx.stderr).to.contain('react')
+    expect(ctx.stderr).to.contain('buttons')
+    expect(ctx.stderr).to.contain('button-1')
   })
 })
