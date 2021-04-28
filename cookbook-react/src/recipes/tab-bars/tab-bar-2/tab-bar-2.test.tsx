aad9aacd3904277df43137b4310f0fa6239bf647
santiagoRD <santi_diaz17@hotmail.com>
2021-04-28 15:47:47 -0500

    create tab bar component, styles and test

---
+++ cookbook-react/src/recipes/tab-bars/tab-bar-2/tab-bar-2.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import TabBar from './index';
+
+const tabs = [
+  { id: 1, label: 'Primera tab', icon: '*' },
+  { id: 2, label: 'Segunda tab', icon: ':D' }
+];
+const active = null;
+const handleChange = jest.fn();
+
+test('call handleChange function with the tab id', () => {
+  render(<TabBar tabs={tabs} active={active} handleChange={handleChange} />);
+  userEvent.click(screen.getByLabelText('Primera tab'));
+  expect(handleChange).toHaveBeenCalledWith(1);
+});
