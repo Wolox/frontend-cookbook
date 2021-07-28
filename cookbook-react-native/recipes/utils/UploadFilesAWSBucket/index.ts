832c54f71d3c8738ea64feafb14dc48b950e278f
mattgle <matias.grote@wolox.com>
2021-07-28 16:06:07 -0300

    add props to readme

--- cookbook-react-native/recipes/utils/UploadFilesAWSBucket/index.ts
+++ cookbook-react-native/recipes/utils/UploadFilesAWSBucket/index.ts
@@ -10,6 +10,12 @@ export enum FileType {
   IMAGE = 'image',
   VIDEO = 'video'
 }
+interface UploadFileToBucketProps {
+  fileUri: string;
+  fileType: FileType;
+  fileUrl: string;
+  dispatch: Dispatch;
+};
 
 const handleErrorUploading = (_: Dispatch) => {
   //TODO: Add your error handler here.
@@ -19,21 +25,6 @@ const getFileType = (type: FileType) =>
   type === FileType.IMAGE ? ContentTypeByFileType.IMAGE : ContentTypeByFileType.VIDEO;
 
 const getRealPath = (file: string) => (Platform.OS === 'ios' ? file.replace('file://', '') : file);
-
-/*
-Documentation for UploadFileToBucketProps interface:
-
-fileUri: is the path where the file is saved.
-fileType: is the type of the file, in this case it can only be an image or a video.
-fileUrl: is the signed url that aws provides to upload the file.
-*/
-
-interface UploadFileToBucketProps {
-  fileUri: string;
-  fileType: FileType;
-  fileUrl: string;
-  dispatch: Dispatch;
-};
 
 export const uploadFileToBucket =  async ({ fileUri, fileType, fileUrl, dispatch }: UploadFileToBucketProps) => {
   let readyToPublish = false;
--- cookbook-react-native/recipes/utils/UploadFilesAWSBucket/readme.md
+++ cookbook-react-native/recipes/utils/UploadFilesAWSBucket/readme.md
@@ -16,3 +16,12 @@ uploadFileToBucket({
       dispatch
     });
 ```
+
+## Props
+
+| Prop  | Type | Description |
+| :------------ | :---------------:| :-----|
+| dispatch | Dispatch | is the dispatch from Redux.
+| fileUri | string | is the path where the file is saved.
+| fileType | FileType | is the type of the file, in this case it can only be an image or a video, you can add more if you need to.
+| fileUrl | string | is the signed url that aws provides to upload the file.
